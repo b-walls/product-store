d0b926561419a7b4f9c6a6626188c01abf62de76
Brendan <brendan.walls22@gmail.com>
2025-04-12 16:51:14 -0400

    Fixed syntax error for Render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Box, useColorModeValue } from "@chakra-ui/react"
 import { Route, Routes } from 'react-router-dom';
 import CreatePage from "./pages/CreatePage";
-import HomePage from "./pages/Homepage";
+import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 
 function App() {  
